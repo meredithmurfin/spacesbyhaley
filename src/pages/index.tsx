9fc1532f41f63a50a34bbee9c46d881bf9c74e6b
Meredith Murfin <62190025+meredithmurfin@users.noreply.github.com>
2021-02-07 01:13:16 -0500

    feat: adds inquiry form link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,21 +5,23 @@ export default function Home() {
   return (
     <Layout>
       <main>
-        <p>
-          Facebook:{' '}
+        <p style={{ fontFamily: 'Arial, serif', textAlign: 'center' }}>
           <a href="https://m.facebook.com/Spaces-by-Haley-105707784852717/">
-            @spacesbyhaley
+            FACEBOOK
           </a>
         </p>
-        <p>
-          Instagram:{' '}
+        <p style={{ fontFamily: 'Arial, serif', textAlign: 'center' }}>
           <a href="https://instagram.com/spacesbyhaley?igshid=c9t7e3t17opm/">
-            @spacesbyhaley
+            INSTAGRAM
           </a>
         </p>
-        <p>
-          Contact:{' '}
-          <a href="mailto:haleyledwitch@gmail.com">haleyledwitch@gmail.com</a>
+        <p style={{ fontFamily: 'Arial, serif', textAlign: 'center' }}>
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSfXEuxM6z_wfwKvPaNxwlu1odorcUsnjRRdDmI5qmUm_-OBrw/viewform">
+            INQUIRY FORM
+          </a>
+        </p>
+        <p style={{ fontFamily: 'Arial, serif', textAlign: 'center' }}>
+          <a href="mailto:haleyledwitch@gmail.com">EMAIL</a>
         </p>
       </main>
     </Layout>
